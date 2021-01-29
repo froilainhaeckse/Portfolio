ad78078d97b65136e16c4bcdd5b4d5e792d025ac
Thea <thea.kupler@liefery.com>
2021-01-29 18:25:22 +0100

    add new and create action for portfolio

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,20 @@ class RecordsController < ApplicationController
   def index
     @record_items = Record.all
   end
+
+  def new
+    @record_item = Record.new
+  end
+
+  def create
+    @record_item = Record.new(params.require(:record).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @record_item.save
+        format.html { redirect_to records_url, notice: 'Portfolio Item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
